033950677d6bb18e89cb7ae5900949d882e06668
Kim Soheui <bigdata391@gmail.com>
2023-12-22 04:37:10 +0900

    edit-logout

--- src/component/Logout.js
+++ src/component/Logout.js
@@ -1,27 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import { IoLogOut } from "react-icons/io5";
+import { logoutDomain } from "./common";
 
 function Logout() {
   const navigate = useNavigate();
 
-  // 쿠키 삭제 함수
-function deleteAllCookies() {
-  var cookies = document.cookie.split(";");
-
-  for (var i = 0; i < cookies.length; i++) {
-    var cookie = cookies[i];
-    var eqPos = cookie.indexOf("=");
-    var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-  }
-}
-
   const logout = () => {
 
     // 함수 호출로 모든 쿠키 삭제
-    deleteAllCookies();
-
-    goHomePage()
+    fetch(logoutDomain)
+      .then(res=>res.json())
+      .then(json=>{
+        console.log(json)
+        goHomePage()
+      })
   }
   const goHomePage = () => {
     navigate(`/`);
--- src/component/common.js
+++ src/component/common.js
@@ -4,5 +4,6 @@ const userDomain = "http://127.0.0.1:4200/user" // User 데이터 요청 url
 const chatDomain = "http://127.0.0.1:4200/chat" // Chat 데이터 요청 url
 const kakaoDomain = "http://localhost:4200/auth/kakao" // Kakao API 로그인 요청 url
 const naverDomain = "http://localhost:4200/auth/naver" // Naver API 로그인 요청 url
+const logoutDomain = "http://localhost:4200/logout" // Logout 로그인 요청 url
 
-export {roomDomain, folderDomain, userDomain, chatDomain, kakaoDomain, naverDomain};
+export {roomDomain, folderDomain, userDomain, chatDomain, kakaoDomain, naverDomain, logoutDomain};
