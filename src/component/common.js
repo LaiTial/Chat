780618ea63ddb6fff5e87cf5dd42556dee76b389
Kim Soheui <bigdata391@gmail.com>
2023-12-22 03:32:21 +0900

    export-error-edit

--- src/component/common.js
+++ src/component/common.js
@@ -5,4 +5,4 @@ const chatDomain = "http://127.0.0.1:4200/chat" // Chat 데이터 요청 url
 const kakaoDomain = "http://localhost:4200/auth/kakao" // Kakao API 로그인 요청 url
 const naverDomain = "http://localhost:4200/auth/naver" // Naver API 로그인 요청 url
 
-export {roomDomain, folderDomain, userDomain, chatDomain, kakaoDomain, naverDomain, loginDomain};
+export {roomDomain, folderDomain, userDomain, chatDomain, kakaoDomain, naverDomain};
